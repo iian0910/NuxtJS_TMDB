c0628bc6a465be86f2491522ddeb0e4c76d8abc1
EPAY-PRESCO\ian.fan <ian.fan@presco.ws>
2024-06-26 14:28:13 +0800

    新增 Title / Loading 元件

---
+++ components/Loading.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="d-flex flex-column justify-content-center align-items-center" style="height: 100vh">
+    <div class="spinner-border text-warning" role="status"></div>
+    <p class="mt-2 text-warning">Loading...</p>
+  </div>
+</template>
---
+++ components/Title.vue
@@ -0,0 +1,28 @@
+<template>
+  <h3 class="block_title my-3">{{ props.text }}</h3>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.block_title {
+  color: white;
+  line-height: 2;
+  &::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 3px;
+    background-color: #ff2c1f;
+    background: linear-gradient(to right, rgba(255,44,31,1) 0%,rgba(168,138,165,0) 60%,rgba(125,185,232,0) 99%);
+  }
+}
+</style>
