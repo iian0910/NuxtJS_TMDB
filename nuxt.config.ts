71b6d55ad820d82084b4eb9443a3bfc0bce31ba0
EPAY-PRESCO\ian.fan <ian.fan@presco.ws>
2024-06-27 16:48:59 +0800

    設定部屬相關資訊

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/NuxtJS_TMDB/' : '/',
+    buildAssetsDir: '/static/'
+  },
   css: ['@/assets/main.scss'],
   postcss: {
     plugins: { autoprefixer: true }
