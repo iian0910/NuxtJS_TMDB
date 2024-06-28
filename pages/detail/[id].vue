ba3a45d90e088d37888b55f6e5f0df134c4382d2
EPAY-PRESCO\ian.fan <ian.fan@presco.ws>
2024-06-28 14:01:54 +0800

    add modal component

--- pages/detail/[id].vue
+++ pages/detail/[id].vue
@@ -22,7 +22,7 @@
           <p class="text-white">電影分類：</p>
           <div class="ms-3">
             <span
-              class="badge rounded-pill bg-warning text-dark me-2 px-3 py-1 d-inline-block"
+              class="badge rounded-pill bg-warning text-dark mb-3 mb-sm-0 me-2 px-3 py-1 d-inline-block"
               v-for="genre in movie.genres"
               :key="genre.id"
             >
@@ -82,13 +82,29 @@
             <p class="card-text text-white">{{ spliceContent(review.content) }}</p>
             <small class="card-title text-end d-block release_date">— {{ review.author }} {{ dateTrans(review.updated_at) }}</small>
             <div class="d-flex justify-content-md-end mt-3">
-              <button type="button" class="btn btn-outline-danger">MORE...</button>
+              <button
+                type="button"
+                class="btn btn-outline-danger"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+                @click="openModal(review.content)"
+              >MORE...</button>
             </div>
           </div>
         </div>
       </div>
     </div>
     <Footer />
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content border border-white bg-black text-white">
+          <div class="modal-body lh-lg">
+            {{reviewContent}}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -107,6 +123,7 @@ const movie = ref([])
 const movieImgs = ref([])
 const movieReviews = ref([])
 const isLoading = ref(true)
+const reviewContent = ref('')
 
 onMounted(() => {
   getDetail()
@@ -151,5 +168,9 @@ const getMovieReviews = async () => {
 
 const goBack = () => {
   router.push({ name: 'index', path:'/' })
+}
+
+const openModal = (review) => {
+  reviewContent.value = review
 }
 </script>
